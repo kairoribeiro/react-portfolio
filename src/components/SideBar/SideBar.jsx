0e4c9549e79f8388e9999f484f73ddb1f5a4604d
Kairo Ribeiro <kairo10@gmail.com>
2022-10-25 13:50:56 -0400

    set logo

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,24 @@
+import { Link, NavLink } from 'react-router-dom';
 import './SideBar.scss'
+import LogoK from '../../assets/images/logo-k.png'
+import LogoSubtitle from '../../assets/images/kairo.png'
+
 
 const SideBar = () => {
-    return ( 
-        <>
-        <h1>SideBar</h1>
-        </>
-     );
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoK} alt="logo" />
+                <img className='sub-logo' src={LogoSubtitle} alt="logoSubtitle" />
+            </Link>
+            <nav>
+                <NavLink>
+
+                </NavLink>
+            </nav>
+        
+        </div>
+    );
 }
- 
+
 export default SideBar;
