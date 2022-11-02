25656e57dd4ee687062300477fc9b2af19b3e50e
Kairo Ribeiro <kairo10@gmail.com>
2022-11-02 19:20:29 -0400

    add skills comp

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -16,6 +16,20 @@ const Skills = (props) => {
                     idx={10}
                 />
             </h1>
+            <p>My skill set includes languages and technologies like Javascript, Python, React, Node, Express, Django, PostgreSQL, MongoDB, Bootstrap, Google OAuth, HTML, and CSS.</p>
+            <div>
+                <img src="/data/html.png" alt="HTML"/>
+                <img src="/data/css.png" alt="CSS"/>
+                <img src="/data/js.png" alt="JAVASCRIPT"/>
+                <img src="/data/bootstrap.png" alt="BOOTSTRAP"/>
+                <img src="/data/mongodb.png" alt="MONGODB"/>
+                <img src="/data/express.png" alt="EXPRESS"/>
+                <img src="/data/node.png" alt="NODEJS"/>
+                <img src="/data/react.png" alt="REACT"/>
+                <img src="/data/python.png" alt="PYTHON"/>
+                <img src="/data/django.png" alt="DJANGO"/>
+                <img src="/data/postgresql.png" alt="POSTGRESQL"/>
+            </div>
         </div>
         <Loader type='ball-pulse' />
         </>
